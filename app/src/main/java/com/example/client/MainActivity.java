3cd948e721cc2cf2d425c35ff5292efa6d678812
novelevon <70190074+raspberry45674@users.noreply.github.com>
2020-09-28 15:46:16 +0900

    android1

--- app/src/main/java/com/example/client/MainActivity.java
+++ app/src/main/java/com/example/client/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
+ 
 import java.io.*;
 import java.lang.*;
 import java.net.InetAddress;
